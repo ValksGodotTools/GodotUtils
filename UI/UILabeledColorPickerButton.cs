3930f36a4cb91fd6b9fd1e2aef168f315bd629e2
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-13 22:01:03 -0500

    Create UILabeledColorPickerButton.cs

---
+++ UI/UILabeledColorPickerButton.cs
@@ -0,0 +1,41 @@
+﻿namespace GodotUtils;
+
+public partial class UILabeledColorPickerButton : UILabeled
+{
+	public Action<Color> ValueChanged { get; set; }
+	private LabeledColorPickerButtonOptions Options { get; set; }
+	private Color Color { get; set; }
+
+	public UILabeledColorPickerButton(LabeledColorPickerButtonOptions options) : base(options)
+	{
+		Options = options;
+	}
+
+	public override void CreateUI(HBoxContainer hbox)
+	{
+		var colorPicker = Options.ColorPickerButton;
+		colorPicker.CustomMinimumSize = new Vector2(100, 0);
+
+		colorPicker.ColorChanged += color =>
+		{
+			Color = color;
+
+			if (!Options.OnlyUpdateOnPopupClosed)
+				ValueChanged?.Invoke(color);
+		};
+
+		colorPicker.PopupClosed += () =>
+		{
+			if (Options.OnlyUpdateOnPopupClosed)
+				ValueChanged?.Invoke(Color);
+		};
+
+		hbox.AddChild(colorPicker);
+	}
+}
+
+public class LabeledColorPickerButtonOptions : LabeledOptions
+{
+	public ColorPickerButton ColorPickerButton       { get; set; } = new ColorPickerButton();
+	public bool              OnlyUpdateOnPopupClosed { get; set; }
+}
