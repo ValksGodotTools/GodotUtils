5364f76a41d0184a74b2e5aa6c81c12bcbbd223d
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-18 01:04:30 -0500

    strip game logic out of library

--- Manager.cs
+++ Manager.cs
@@ -10,65 +10,7 @@ global using System.Threading.Tasks;
 
 namespace GodotUtils;
 
-// NuGet Package: https://www.nuget.org/packages/ValksGodotUtils/
-// No longer being maintained or used but kept here just in case
-
-public partial class Manager<TPlayerData> : Node
+public class Manager
 {
-    private ENetGameServer<TPlayerData> Server { get; set; }
-    private ENetGameClient Client { get; set; }
-
-    protected void PreInit(ENetGameServer<TPlayerData> server, ENetGameClient client)
-    {
-        Server = server;
-        Client = client;
-    }
-
-    public override async void _PhysicsProcess(double delta)
-    {
-        await Logger.Update();
-    }
-
-    public override async void _Notification(int what)
-    {
-        if (what == NotificationWMCloseRequest)
-        {
-            GetTree().AutoAcceptQuit = false;
-            await Cleanup();
-        }
-    }
-
-    private async Task Cleanup()
-    {
-        await CleanupNet();
-
-        if (Logger.StillWorking())
-            await Task.Delay(1);
-
-        GetTree().Quit();
-    }
-
-    private async Task CleanupNet()
-    {
-        if (ENetLow.ENetInitialized)
-        {
-            if (Client != null)
-            {
-                Client.Stop();
-
-                while (Client.IsRunning)
-                    await Task.Delay(1);
-            }
-
-            if (Server != null)
-            {
-                Server.Stop();
-
-                while (Server.IsRunning)
-                    await Task.Delay(1);
-            }
-
-            ENet.Library.Deinitialize();
-        }
-    }
+    
 }
