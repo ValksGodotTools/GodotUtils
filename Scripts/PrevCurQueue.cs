35daa95d10e4f415018f5ea222a8ee2632ddefc0
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-23 20:17:18 -0500

    Small refactor + add lots of comments to PrevCurQueue

--- Scripts/PrevCurQueue.cs
+++ Scripts/PrevCurQueue.cs
@@ -2,13 +2,20 @@
 
 public class PrevCurQueue<T>
 {
-    private List<T> Data = new();
-    public double Progress { get; private set; }
+    private List<T> Data { get; } = new();
+    public float Progress { get; private set; }
 
     public T Previous { get; set; }
     public T Current { get; set; }
 
-    public bool NotReady => Data.Count <= 1;
+    /*
+     * Should this keep updating even after a Progress of 1.0 has been reached?
+     * If this is set to true then if say a position packet is not received in
+     * time, then the player will continue moving in the last known direction.
+     * If this is set to false then the player will just come to a direct halt
+     * when a Progress value of 1.0 is reached.
+     */
+    public bool KeepUpdating { get; set; }
 
     private int Interval { get; set; }
 
@@ -38,5 +45,32 @@ public class PrevCurQueue<T>
         }
     }
 
-    public void UpdateProgress(double delta) => Progress += delta * (1000d / Interval); // reach value of 1.0 every 150ms
+    /*
+     * There are 60 Frames in 1 Second (60 FPS)
+     * 
+     * If we set Interval to 1000 then UpdateProgress will have to be
+     * called 60 times for Progress to reach a value of 1.0
+     * That is it will happen in 1 second
+     * 
+     * If we set Interval to 2000 then UpdateProgress will have to be
+     * called 120 times for Progress to reach a value of 1.0
+     * That is it will happen in 2 seconds
+     * 
+     * Interval is the update interval. So if the server and client are
+     * both sending say position updates every 50ms, then the Interval
+     * should be set to 50ms
+     */
+    public void UpdateProgress(double delta)
+    {
+        if (Progress < 1.0)
+            AddToProgress(delta);
+        else
+        {
+            if (KeepUpdating)
+                AddToProgress(delta);
+        }
+    }
+
+    private void AddToProgress(double delta) =>
+        Progress += (float)delta * (1000f / Interval);
 }
