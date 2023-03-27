a71d552faa6fc6ed24d8eeb5e2d1e72169bc9633
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-27 01:25:04 -0500

    SetCollisionMaskLayer(...)

---
+++ Scripts/Extensions/ExtensionsCollisionObject.cs
@@ -0,0 +1,21 @@
+﻿namespace GodotUtils;
+
+public static class ExtensionsCollisionObject
+{
+    /// <summary>
+    /// Set the provided layer and mask values to true. Everything else
+    /// will be set to be false.
+    /// </summary>
+    public static void SetCollisionMaskLayer(this CollisionObject2D node, params int[] values)
+    {
+        // Reset all layer and mask values to 0
+        node.CollisionLayer = 0;
+        node.CollisionMask = 0;
+
+        foreach (var value in values)
+        {
+            node.SetCollisionLayerValue(value, true);
+            node.SetCollisionMaskValue(value, true);
+        }
+    }
+}
