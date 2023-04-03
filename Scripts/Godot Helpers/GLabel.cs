c467f85d7af4f75a70c2284ea0ad311e01250a6f
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-03 12:28:58 -0500

    Create GLabel.cs

---
+++ Scripts/Godot Helpers/GLabel.cs
@@ -0,0 +1,12 @@
+﻿namespace GodotUtils;
+
+public partial class GLabel : Label
+{
+    public GLabel(string text, int fontSize = 16)
+    {
+        Text = text;
+        SetFontSize(fontSize);
+    }
+
+    public void SetFontSize(int v) => AddThemeFontSizeOverride("font_size", v);
+}
