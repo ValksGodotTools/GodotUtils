38cdd10a0a5bcaed1bc5b6c45465b3ac9da60f31
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-03 12:33:38 -0500

    Create GWindow.cs

---
+++ Scripts/Godot Helpers/GWindow.cs
@@ -0,0 +1,9 @@
+﻿namespace GodotUtils;
+
+public static class GWindow
+{
+    public static void SetTitle(string title) => DisplayServer.WindowSetTitle(title);
+    
+    public static int GetWidth() => DisplayServer.WindowGetSize().X;
+    public static int GetHeight() => DisplayServer.WindowGetSize().Y;
+}
