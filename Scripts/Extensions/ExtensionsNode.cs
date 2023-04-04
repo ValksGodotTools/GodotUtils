12df7a7f5039873c560c0d9603c310ec21653751
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-04 18:19:20 -0500

    Update ExtensionsNode.cs

--- Scripts/Extensions/ExtensionsNode.cs
+++ Scripts/Extensions/ExtensionsNode.cs
@@ -2,6 +2,20 @@ namespace GodotUtils;
 
 public static class ExtensionsNode 
 {
+    /// <summary>
+    /// Get all children assuming they all extend from TNode
+    /// </summary>
+    public static TNode[] GetChildren<TNode>(this Node parent) where TNode : Node
+    {
+        var children = parent.GetChildren();
+        var arr = new TNode[children.Count];
+
+        for (int i = 0; i < children.Count; i++)
+            arr[i] = (TNode)children[i];
+
+        return arr;
+    }
+
     public static void QueueFreeChildren(this Node parentNode)
     {
         foreach (Node node in parentNode.GetChildren())
