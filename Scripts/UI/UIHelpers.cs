b15cfd34f59239ce8e723bebe625c1bf1faacc2f
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-04 18:57:15 -0500

    Create UIHelpers.cs

---
+++ Scripts/UI/UIHelpers.cs
@@ -0,0 +1,33 @@
+﻿namespace GodotUtils;
+
+public static class UIHelpers
+{
+    /// <summary>
+    /// Create several sliders for debugging purposes
+    /// </summary>
+    public static void CreateDebugSliders(Node parent, 
+        Action<float>[] valueChanged, int minValue = -1000, int maxValue = 1000)
+    {
+        var vbox = new VBoxContainer();
+        vbox.Name = "VBox";
+        vbox.AddThemeConstantOverride("separation", 0);
+
+        parent.AddChild(vbox);
+
+        for (int i = 0; i < valueChanged.Count(); i++)
+        {
+            var slider = new UISlider(new SliderOptions
+            {
+                Name = "Debug",
+                HSlider = new HSlider
+                {
+                    MinValue = minValue,
+                    MaxValue = maxValue
+                },
+                HideBackPanel = false
+            });
+            slider.ValueChanged += valueChanged[i];
+            vbox.AddChild(slider);
+        }
+    }
+}
