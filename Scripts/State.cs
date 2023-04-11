5789ee5a73faaca87d3c93666fec382c6b9d9a77
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-11 18:15:10 -0500

    Create State.cs

---
+++ Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using GodotUtils;
+
+public abstract class State<TEntity> where TEntity : IStateMachine<TEntity>
+{
+    protected TEntity Entity { get; set; }
+
+    public State(TEntity entity) => Entity = entity;
+
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void Update();
+
+    public void SwitchState(object newState)
+    {
+        Entity.States[Entity.CurrentState].ExitState();
+        Entity.CurrentState = newState;
+        Entity.States[Entity.CurrentState].EnterState();
+    }
+}
+
+public interface IStateMachine<TEntity> where TEntity : IStateMachine<TEntity>
+{
+    public Dictionary<object, State<TEntity>> States { get; set; }
+    public object CurrentState { get; set; }
+}
