6ea096b65d3e9ee07ca8e5b8602c6565f0cd7cc9
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-11 20:13:03 -0500

    Reimplement GTween

---
+++ Scripts/Godot Helpers/GTween.cs
@@ -0,0 +1,28 @@
+﻿namespace GodotUtils;
+
+public class GTween
+{
+    private Tween Tween { get; set; }
+    private Node  Node  { get; }
+
+    public GTween(Node node) => Node = node;
+
+    public void Create()
+    {
+        Kill();
+        Tween = Node.CreateTween();
+    }
+
+    public PropertyTweener Animate(NodePath prop, Variant finalValue, double duration, bool parallel = false)
+    {
+        if (parallel)
+            return Tween.Parallel().TweenProperty(Node, prop, finalValue, duration);
+        else
+            return Tween.TweenProperty(Node, prop, finalValue, duration);
+    }
+
+    public void Callback(Action callback) =>
+        Tween.TweenCallback(Callable.From(callback));
+
+    public void Kill() => Tween?.Kill();
+}
