a114c4f98b7bd70cc52b76598604b644f3cd6644
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-19 16:42:48 -0500

    Create GPadding.cs

---
+++ Scripts/Godot Helpers/GPadding.cs
@@ -0,0 +1,9 @@
+﻿namespace GodotUtils;
+
+public partial class GPadding : Control
+{
+    public GPadding(int paddingX = 0, int paddingY = 0)
+    {
+        CustomMinimumSize = new Vector2(paddingX, paddingY);
+    }
+}
