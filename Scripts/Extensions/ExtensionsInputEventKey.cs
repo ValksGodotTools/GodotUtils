57053b216f2bc4bfdcaca999dfc3247fe9001481
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-22 20:21:09 -0500

    Update ExtensionsInputEventKey.cs

--- Scripts/Extensions/ExtensionsInputEventKey.cs
+++ Scripts/Extensions/ExtensionsInputEventKey.cs
@@ -2,9 +2,23 @@
 
 public static class ExtensionsInputEventKey
 {
-    public static bool IsKeyJustPressed(this InputEventKey inputEventKey, Key key) =>
-        inputEventKey.Keycode == key && inputEventKey.Pressed && !inputEventKey.Echo;
+    public static bool IsKeyJustPressed(this InputEventKey v, Key key) =>
+        v.Keycode == key && v.Pressed && !v.Echo;
 
-    public static bool IsKeyJustReleased(this InputEventKey inputEventKey, Key key) =>
-        inputEventKey.Keycode == key && !inputEventKey.Pressed && !inputEventKey.Echo;
+    public static bool IsKeyJustReleased(this InputEventKey v, Key key) =>
+        v.Keycode == key && !v.Pressed && !v.Echo;
+
+    /// <summary>
+    /// <para>Convert to a human readable key</para>
+    /// <para>For example 'Ctrl + Shift + E'</para>
+    /// </summary>
+    public static string Readable(this InputEventKey v)
+    {
+        // If Keycode is not set than use PhysicalKeycode
+        var keyWithModifiers = v.Keycode == Key.None ?
+            v.GetPhysicalKeycodeWithModifiers() :
+            v.GetKeycodeWithModifiers();
+
+        return OS.GetKeycodeString(keyWithModifiers).Replace("+", " + ");
+    }
 }
