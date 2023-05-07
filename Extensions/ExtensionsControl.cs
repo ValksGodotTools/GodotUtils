387e40d1c1232fa166d728c967a1d5ad1774fd47
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-07 13:45:48 -0500

    Create ExtensionsControl.cs

---
+++ Extensions/ExtensionsControl.cs
@@ -0,0 +1,10 @@
+﻿namespace GodotUtils;
+
+public static class ExtensionsControl
+{
+    public static void CoverEntireScreen(this Control control) =>
+        control.SetAnchorsAndOffsetsPreset(Control.LayoutPreset.FullRect);
+
+    public static void CenterToScreen(this Control control) =>
+        control.SetAnchorsAndOffsetsPreset(Control.LayoutPreset.Center);
+}
