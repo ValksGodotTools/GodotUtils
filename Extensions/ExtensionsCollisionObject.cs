10c6ce4798b8f2844fb5e89ce52a0b8a710aed3d
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-10 01:57:04 -0500

    Update ExtensionsCollisionObject.cs

--- Extensions/ExtensionsCollisionObject.cs
+++ Extensions/ExtensionsCollisionObject.cs
@@ -3,10 +3,27 @@
 public static class ExtensionsCollisionObject
 {
     /// <summary>
-    /// Set the provided layer and mask values to true. Everything else
-    /// will be set to be false.
+    /// Set the specified layer and mask values to true. Everything else will 
+    /// be set to be false.
     /// </summary>
     public static void SetCollisionMaskLayer(this CollisionObject2D node, params int[] values)
+    {
+        // Reset all layer and mask values to 0
+        node.CollisionLayer = 0;
+        node.CollisionMask = 0;
+
+        foreach (var value in values)
+        {
+            node.SetCollisionLayerValue(value, true);
+            node.SetCollisionMaskValue(value, true);
+        }
+    }
+
+    /// <summary>
+    /// Set the specified layer and mask values to true. Everything else will 
+    /// be set to be false.
+    /// </summary>
+    public static void SetCollisionMaskLayer(this CharacterBody2D node, params int[] values)
     {
         // Reset all layer and mask values to 0
         node.CollisionLayer = 0;
