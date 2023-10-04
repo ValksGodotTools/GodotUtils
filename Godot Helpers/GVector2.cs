bb95db03639a9ba4826c38279892ca0b973a9ac7
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-10-04 16:19:34 -0500

    Create GVector2.cs

---
+++ Godot Helpers/GVector2.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceGame;
+
+public static class GVector2
+{
+    /// <summary>
+    /// Returns a random vector between 0 and 1 (inclusive) for X and Y.
+    /// </summary>
+    public static Vector2 Random() => new(GD.Randf(), GD.Randf());
+}
