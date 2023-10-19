4ee4658e1d2429b5cf466d94d049613b0d5b6f15
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-10-18 23:59:25 -0500

    Create ServiceProvider.cs

---
+++ ServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace GodotUtils;
+
+// Solves the need for 'static'
+public class ServiceProvider
+{
+    Dictionary<Type, object> services = new();
+
+    public void AddService(object service) =>
+        services.Add(service.GetType(), service);
+
+    public T GetService<T>() => (T)services[typeof(T)];
+}
