1a9e6c424fe7267dcaea6474367d7cfbb3ce9c8c
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-11-05 23:17:33 -0600

    Update GVector2.cs

--- Godot Helpers/GVector2.cs
+++ Godot Helpers/GVector2.cs
@@ -5,5 +5,6 @@ public static class GVector2
     /// <summary>
     /// Returns a random vector between 0 and 1 (inclusive) for X and Y.
     /// </summary>
-    public static Vector2 Random() => new(GD.Randf(), GD.Randf());
+    public static Vector2 Random() => 
+        new Vector2((float)GD.RandRange(-1.0, 1.0), (float)GD.RandRange(-1.0, 1.0)).Normalized();
 }
