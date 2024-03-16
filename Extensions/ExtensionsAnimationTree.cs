7b3a89b3faf7d0783676b7d212743c393fa114ea
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-03-16 01:20:04 -0500

    Create ExtensionsAnimationTree.cs

---
+++ Extensions/ExtensionsAnimationTree.cs
@@ -0,0 +1,12 @@
+﻿namespace GodotUtils;
+
+using Godot;
+
+public static class ExtensionsAnimationTree
+{
+    public static void SetCondition(this AnimationTree tree, StringName condition, Variant value) =>
+        tree.Set($"parameters/conditions/{condition}", value);
+
+    public static void SetParam(this AnimationTree tree, StringName path, Variant value) =>
+        tree.Set($"parameters/{path}", value);
+}
