00ea82a5094ffb7df4f633f14ac5f81ca4e035cb
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-03-20 18:31:09 -0500

    GetRaycastsColliding() extension

--- Extensions/ExtensionsRayCast3D.cs
+++ Extensions/ExtensionsRayCast3D.cs
@@ -4,6 +4,19 @@ using Godot;
 
 public static class ExtensionsRayCast3D
 {
+    public static int GetRaycastsColliding(this List<RayCast3D> raycasts)
+    {
+        int numRaycastsColliding = 0;
+
+        foreach (RayCast3D raycast in raycasts)
+        {
+            if (raycast.IsColliding())
+                numRaycastsColliding++;
+        }
+
+        return numRaycastsColliding;
+    }
+
     public static void ExcludeRaycastParents(this RayCast3D raycast) =>
         ExcludeParents(raycast, raycast.GetParent());
 
