99e15c3150c512c3c844850458c73bad1a7161e1
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-06-04 20:23:10 -0500

    Create ExtensionsDictionary.cs

---
+++ Extensions/ExtensionsDictionary.cs
@@ -0,0 +1,12 @@
+﻿namespace GodotUtils;
+
+public static class ExtensionsDictionary
+{
+    public static void Merge<TKey, TValue>(this Dictionary<TKey, TValue> me, Dictionary<TKey, TValue> merge)
+    {
+        foreach (var item in merge)
+        {
+            me[item.Key] = item.Value;
+        }
+    }
+}
