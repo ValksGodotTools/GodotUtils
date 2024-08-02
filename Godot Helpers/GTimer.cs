daf37fa95ab3f099fc303716c98037bb78bb8638
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-01 23:08:41 -0500

    Create GTimer.cs

---
+++ Godot Helpers/GTimer.cs
@@ -0,0 +1,23 @@
+﻿namespace GodotUtils;
+
+using Godot;
+using System;
+
+public class GTimer
+{
+    public event Action Timeout;
+
+    private Timer timer;
+
+    public GTimer(Node node, double seconds, bool looping)
+    {
+        timer = new Timer();
+        timer.ProcessCallback = Timer.TimerProcessCallback.Physics;
+        timer.OneShot = !looping;
+        timer.WaitTime = seconds;
+        node.AddChild(timer);
+        timer.Timeout += () => Timeout?.Invoke();
+    }
+
+    public void Start() => timer.Start();
+}
