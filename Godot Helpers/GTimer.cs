200ae730aca9a831d0cfbe467ffeb11d6d58d14c
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-04 18:54:41 -0500

    Update GTimer.cs

--- Godot Helpers/GTimer.cs
+++ Godot Helpers/GTimer.cs
@@ -9,12 +9,12 @@ public class GTimer
 
     private Timer timer;
 
-    public GTimer(Node node, double seconds, bool looping)
+    public GTimer(Node node, double milliseconds, bool looping)
     {
         timer = new Timer();
         timer.ProcessCallback = Timer.TimerProcessCallback.Physics;
         timer.OneShot = !looping;
-        timer.WaitTime = seconds;
+        timer.WaitTime = milliseconds * 0.001; // Convert from milliseconds to seconds
         node.AddChild(timer);
         timer.Timeout += () => Timeout?.Invoke();
     }
