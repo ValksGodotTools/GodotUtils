33f6b152805a709a60b1cc655bd27af805566737
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-29 00:20:01 -0500

    Create GDirectories.cs

---
+++ Utilities/GDirectories.cs
@@ -0,0 +1,40 @@
+﻿namespace GodotUtils;
+
+using Godot;
+using System.IO;
+
+public static class GDirectories
+{
+    /// <summary>
+    /// Recursively traverses all directories and performs a action on each file path. This ignores
+    /// any directories that start with a period like .godot
+    /// 
+    /// <code>
+    /// GDirectories.Traverse("res://", fullFilePath => GD.Print(fullFilePath))
+    /// </code>
+    /// </summary>
+    public static void Traverse(string relativeFolder, Action<string> actionFullFilePath)
+    {
+        using DirAccess dir = DirAccess.Open(relativeFolder);
+
+        dir.ListDirBegin();
+
+        string fileName;
+
+        while ((fileName = dir.GetNext()) != "")
+        {
+            string fullFilePath = Path.Combine(ProjectSettings.GlobalizePath(relativeFolder), fileName);
+
+            if (dir.CurrentIsDir() && !fileName.StartsWith("."))
+            {
+                Traverse(fullFilePath, actionFullFilePath);
+            }
+            else
+            {
+                actionFullFilePath(fullFilePath);
+            }
+        }
+
+        dir.ListDirEnd();
+    }
+}
