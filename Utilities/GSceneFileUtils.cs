4e0aaf1881c85c387676e16597da548ce30a682f
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-29 02:13:40 -0500

    Update GSceneFileUtils.cs

--- Utilities/GSceneFileUtils.cs
+++ Utilities/GSceneFileUtils.cs
@@ -6,41 +6,38 @@ public static class GSceneFileUtils
 {
     public static void FixBrokenDependencies()
     {
-        FixBrokenResourcePaths("res://");
+        GDirectories.Traverse(ProjectSettings.GlobalizePath("res://"), FixBrokenResourcePath);
     }
 
-    private static void FixBrokenResourcePaths(string relativeFolder)
+    private static void FixBrokenResourcePath(string fullFilePath)
     {
-        GDirectories.Traverse(relativeFolder, fullFilePath =>
+        if (fullFilePath.EndsWith(".tscn"))
         {
-            if (fullFilePath.EndsWith(".tscn"))
+            string text = File.ReadAllText(fullFilePath);
+
+            Regex regex = new("path=\"(?<path>.+)\" ");
+
+            foreach (Match match in regex.Matches(text))
             {
-                string text = File.ReadAllText(fullFilePath);
+                string oldResourcePath = match.Groups["path"].Value;
 
-                Regex regex = new("path=\"(?<path>.+)\" ");
-
-                foreach (Match match in regex.Matches(text))
+                if (!Godot.FileAccess.FileExists(oldResourcePath))
                 {
-                    string oldResourcePath = match.Groups["path"].Value;
+                    string newResourcePath = FindLocalPathToFile("res://", oldResourcePath.GetFile());
 
-                    if (!Godot.FileAccess.FileExists(oldResourcePath))
+                    if (!string.IsNullOrWhiteSpace(newResourcePath))
                     {
-                        string newResourcePath = FindLocalPathToFile("res://", oldResourcePath.GetFile());
-
-                        if (!string.IsNullOrWhiteSpace(newResourcePath))
-                        {
-                            text = text.Replace(oldResourcePath, newResourcePath);
-                        }
-                        else
-                        {
-                            GD.Print($"Failed to fix a resource path for the scene '{fullFilePath.GetFile()}'. The resource '{oldResourcePath.GetFile()}' could not be found in the project.");
-                        }
+                        text = text.Replace(oldResourcePath, newResourcePath);
+                    }
+                    else
+                    {
+                        GD.Print($"Failed to fix a resource path for the scene '{fullFilePath.GetFile()}'. The resource '{oldResourcePath.GetFile()}' could not be found in the project.");
                     }
                 }
-
-                File.WriteAllText(fullFilePath, text);
             }
-        });
+
+            File.WriteAllText(fullFilePath, text);
+        }
     }
 
     private static string FindLocalPathToFile(string relativeFolder, string fileName)
@@ -49,12 +46,20 @@ public static class GSceneFileUtils
 
         GDirectories.Traverse(relativeFolder, fullFilePath =>
         {
-            if (fullFilePath.GetFile() == fileName)
-            {
-                file = ProjectSettings.LocalizePath(fullFilePath);
-            }
+            file = CheckIsFile(fullFilePath, fileName);
         });
 
         return file;
+    }
+
+    private static string CheckIsFile(string fullFilePath, string fileName)
+    {
+        if (fullFilePath.GetFile() == fileName)
+        {
+            string localPath = ProjectSettings.LocalizePath(fullFilePath);
+            return localPath;
+        }
+
+        return null;
     }
 }
