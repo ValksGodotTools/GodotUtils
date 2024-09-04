7220a270a0aeaceaba9c35b63d0fc27dddc5e4eb
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-04 17:45:26 -0500

    Create ExtensionsSceneTree.cs

---
+++ Extensions/ExtensionsSceneTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Godot;
+
+namespace GodotUtils;
+
+public static class ExtensionsSceneTree
+{
+    /// <summary>
+    /// Unfocus the current UI element that is being focused
+    /// </summary>
+    public static void UnfocusCurrentControl(this SceneTree tree)
+    {
+        // Get the currently focused control
+        Control focusedControl = tree.Root.GuiGetFocusOwner();
+
+        if (focusedControl != null)
+        {
+            // Set the focus mode to None to unfocus it
+            focusedControl.FocusMode = Control.FocusModeEnum.None;
+        }
+    }
+}
