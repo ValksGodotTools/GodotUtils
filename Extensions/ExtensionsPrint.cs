d6e7cf4ebfd71e4ff73a2323448854deb16741d3
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-07 16:17:17 -0500

    Update ExtensionsPrint.cs

--- Extensions/ExtensionsPrint.cs
+++ Extensions/ExtensionsPrint.cs
@@ -8,20 +8,59 @@ using System;
 
 namespace GodotUtils;
 
+/// <summary>
+/// Provides extension methods for printing objects and collections in a formatted manner.
+/// </summary>
 public static class ExtensionsPrint
 {
     /// <summary>
-    /// Prints a collection in a readable format
+    /// Prints the elements of an enumerable collection in a formatted string.
     /// </summary>
-    public static string Print<T>(this IEnumerable<T> value, bool newLine = true) =>
-        value != null ? string.Join(newLine ? "\n" : ", ", value) : null;
+    /// <typeparam name="T">The type of elements in the enumerable collection.</typeparam>
+    /// <param name="value">The enumerable collection to print.</param>
+    /// <param name="newLine">If true, each element will be printed on a new line; otherwise, elements will be printed on the same line separated by commas.</param>
+    public static void PrintFormatted<T>(this IEnumerable<T> value, bool newLine = true)
+    {
+        GD.Print(value.ToFormattedString(newLine));
+    }
 
     /// <summary>
-    /// Prints the entire object in a readable format (supports Godot properties)
-    /// If you should ever run into a problem, see the IgnorePropsResolver class to ignore more
-    /// properties.
+    /// Converts the object to a formatted JSON string and prints it.
     /// </summary>
-    public static string PrintFull(this object v) =>
+    /// <param name="v">The object to print.</param>
+    public static void PrintFormatted(this object v)
+    {
+        GD.Print(v.ToFormattedString());
+    }
+
+    /// <summary>
+    /// Converts the elements of an enumerable collection to a formatted string.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the enumerable collection.</typeparam>
+    /// <param name="value">The enumerable collection to convert.</param>
+    /// <param name="newLine">If true, each element will be printed on a new line; otherwise, elements will be printed on the same line separated by commas.</param>
+    /// <returns>A formatted string representation of the enumerable collection.</returns>
+    public static string ToFormattedString<T>(this IEnumerable<T> value, bool newLine = true)
+    {
+        if (value == null)
+            return null;
+
+        if (newLine)
+        {
+            return "[\n    " + string.Join(",\n    ", value) + "\n]";
+        }
+        else
+        {
+            return "[" + string.Join(", ", value) + "]";
+        }
+    }
+
+    /// <summary>
+    /// Converts the object to a formatted JSON string.
+    /// </summary>
+    /// <param name="v">The object to convert.</param>
+    /// <returns>A formatted JSON string representation of the object.</returns>
+    public static string ToFormattedString(this object v) =>
         JsonConvert.SerializeObject(v, Formatting.Indented, new JsonSerializerSettings
         {
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
@@ -29,30 +68,29 @@ public static class ExtensionsPrint
         });
 
     /// <summary>
-    /// Similar to the PrintFull() function except the contents are printed with GD.Print()
+    /// A custom contract resolver used to ignore certain Godot properties during JSON serialization.
     /// </summary>
-    public static void GDPrintFull(this object v) =>
-        GD.Print(v.PrintFull());
-
-    /// <summary>
-    /// Used when doing JsonConvert.SerializeObject to ignore Godot properties
-    /// as these are massive.
-    /// </summary>
-    class IgnorePropsResolver : DefaultContractResolver
+    private class IgnorePropsResolver : DefaultContractResolver
     {
+        /// <summary>
+        /// Creates a JsonProperty for the given MemberInfo.
+        /// </summary>
+        /// <param name="member">The MemberInfo to create a JsonProperty for.</param>
+        /// <param name="memberSerialization">The MemberSerialization mode for the member.</param>
+        /// <returns>A JsonProperty for the given MemberInfo.</returns>
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
             JsonProperty prop =
                 base.CreateProperty(member, memberSerialization);
 
             // Ignored properties (prevents crashes)
-            Type[] ignoredProps = new Type[]
-            {
+            Type[] ignoredProps =
+            [
                 typeof(GodotObject),
                 typeof(Node),
                 typeof(NodePath),
                 typeof(ENet.Packet)
-            };
+            ];
 
             foreach (Type ignoredProp in ignoredProps)
             {
@@ -67,4 +105,3 @@ public static class ExtensionsPrint
         }
     }
 }
-
