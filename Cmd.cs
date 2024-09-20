76109d2ed2adcc391407fb94d289a999887ddb53
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-20 01:27:18 -0500

    Delete Cmd.cs

--- Cmd.cs
+++
@@ -1,8 +0,0 @@
-namespace GodotUtils;
-
-public class Cmd<TOpcode>(TOpcode opcode, params object[] data)
-{
-    public TOpcode Opcode { get; set; } = opcode;
-    public object[] Data { get; set; } = data;
-}
-
